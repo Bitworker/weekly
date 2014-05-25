59cabbdd05b3d5094a37463a847ffaba273105ba
bitworker.dev@gmail.com <stefan@VirtualBox.(none)>
2014-05-25 12:09:58 +0200

    Forget files

---
+++ app/controllers/interest_controller.rb
@@ -0,0 +1,44 @@
+class InterestController < ApplicationController
+  def create
+    @interest = Interest.new(interest_params)
+    
+    if @interest.save
+      if params[:page] == "company"
+        @user         = User.new
+        @user.email   = @interest.email
+        @user.role    = :pending
+        @user.company = current_user.company
+        
+        if @user.save
+          flash[:notice] = "Mitarbeiter #{@user.email} wurde hinzugefügt."
+        else
+          flash[:alert] = "Mitarbeiter konnte nicht hinzugefügt werden."
+        end
+        
+        redirect_to :back
+      elsif params[:page] == "welcome"
+        redirect_to new_user_registration_path(:email => @interest.email)
+      else
+        raise "Unknown controller for Interest/Create"
+      end  
+    else
+      if params[:page] == "company"
+        flash[:alert] = "Mitarbeiter konnte nicht hinzugefügt werden."
+        
+        redirect_to :back
+      elsif params[:page] == "welcome"
+        flash[:alert] = "Account konnte nicht erstellt werden."
+        
+        render index
+      else
+        raise "Unknown controller for Interest/Create"
+      end
+    end
+  end
+  
+  protected
+  
+  def interest_params
+    params.require(:interest).permit(:email)
+  end
+end
